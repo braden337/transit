0a17ef8666f2c8a8786c78deec6c9dbb4f16b7b1
Braden Jonsson <braden337@gmail.com>
2020-12-17 11:56:30 -0600

    Search for streets

--- src/main.js
+++ src/main.js
@@ -2,3 +2,51 @@ import 'normalize.css';
 import './style.css';
 
 require('dotenv').config();
+
+const winnipegTransitURL = new URL('https://api.winnipegtransit.com/v3');
+const searchParams = new URLSearchParams();
+searchParams.append('api-key', process.env.APIKEY);
+
+const noStreetsFound = `<div class="no-results">No Streets Found</div>`;
+
+const streetSection = document.querySelector('.streets');
+
+search.onsubmit = handleStreetSearch;
+
+function handleStreetSearch(submitEvent) {
+  submitEvent.preventDefault();
+  const data = new FormData(search);
+  const name = data.get('street-name');
+
+  if (name !== '') {
+    getStreets(name);
+    search.reset();
+  }
+}
+
+function getStreets(name) {
+  streetSection.innerHTML = '';
+
+  searchParams.append('name', name);
+  searchParams.append('usage', 'long');
+
+  winnipegTransitURL.search = searchParams;
+  winnipegTransitURL.pathname = '/v3/streets.json';
+
+  const url = winnipegTransitURL.href;
+
+  searchParams.delete('name');
+  searchParams.delete('usage');
+
+  fetch(url)
+    .then(response => response.json())
+    .then(result => showStreets(result.streets));
+}
+
+const streetToHTML = street =>
+  `<a href="javascript:void(0);" data-street-key="${street.key}">${street.name}</a>`;
+
+function showStreets(streets) {
+  streetSection.innerHTML =
+    streets.length > 0 ? streets.map(streetToHTML).join('') : noStreetsFound;
+}
